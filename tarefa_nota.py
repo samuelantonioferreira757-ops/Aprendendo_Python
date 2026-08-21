b93754c2d1c972e9b4f139d68c44d8ccce68c6ef
samuelantonioferreira757-ops <samuelantonioferreira757@gmail.com>
2026-08-20 20:04:07 -0400

    Add files via upload

---
+++ tarefa_nota.py
@@ -0,0 +1,16 @@
+
+""" n1 = 8.5
+n2 = 1.0
+n3 = 2.5
+n4 = 3
+
+Media= (n1+n2+n3+n4)/4
+
+if Media >= 7.0:
+    print("Aprovado")
+elif Media >= 5.0:
+    print("recuperação")    
+else:
+    print("Reprovado")
+    
+     """
