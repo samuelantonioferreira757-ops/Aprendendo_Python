e913b5403538bb1017b7f0db3ffc9e3f25e35667
samuelantonioferreira757-ops <samuelantonioferreira757@gmail.com>
2026-08-20 21:25:33 -0400

    lista

---
+++ lista.py
@@ -0,0 +1,14 @@
+componetes= ["Teclado","Mouse","Monitor"]
+
+
+componetes. append ("Gabinete")
+componetes.remove("Mouse")
+print(componetes)
+print(componetes[0])
+print (componetes[-1])
+
+for i in range(3):
+    if i == 0:
+        print(componetes[i])
+
+
