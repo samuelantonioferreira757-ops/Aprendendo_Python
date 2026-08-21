e9d9a77c900add1de9fd279554a4f83d6aabfed8
samuelantonioferreira757-ops <samuelantonioferreira757@gmail.com>
2026-08-20 20:10:48 -0400

    foi alterado alguns texto e adicionado, outras funções.

--- estrutura_de_repeticao_for.py
+++ estrutura_de_repeticao_for.py
@@ -1,8 +1,27 @@
-computador=["Processador","Teclado","Mouse",]
+""" computador=["Processador","Teclado","Mouse",]
 
 
 for item in computador:
     if item == "Mouse":
-        print(computador[1])
+        print(computador[1])"""
 
 
+""" for i in range(7,21,7):
+    print(i)
+ """
+""" 
+for letra in "Python":
+    print(letra) """
+
+aluno = {"nome":"João","idade":20}
+
+print(aluno.items())
+for chave, valor in aluno.items():
+    print(chave,":",valor)
+    print(f"{chave}:{valor}")
+
+soma= 0
+for x in range(1,4):
+    print("X:",x)
+    soma+=x
+    print("Soma:",soma)
